f2a4c8e43a204436271e02e8076582b72e187d8e
uma-dev <2565959+uma-dev@users.noreply.github.com>
2025-07-16 12:31:20 -0600

    first version personal page

---
+++ src/app/oroldan/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+export default function MyPage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Hello from oroldan!</h1>
+      <h1 className="text-2xl font-bold mb-4">Hello from oroldan!</h1>
+      <Button>Click Me</Button>
+    </div>
+  )
+}
